dac2d735b541524d0d64f138499c231c3342644c
sting1023 <128615011+sting1023@users.noreply.github.com>
2026-03-31 00:11:46 +0800

    chore: sync v1.0.1 - app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,4 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.gridlayout:gridlayout:1.0.0")
 }
